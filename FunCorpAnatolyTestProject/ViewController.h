fdca4107f8bef65280c2aa868808185b1e769a6b
steelhaus <steelhaus141294@gmail.com>
2018-09-06 23:06:07 +0300

    Initial Commit

---
+++ FunCorpAnatolyTestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunCorpAnatolyTestProject
+//
+//  Created by Анатолий Карельский on 06.09.18.
+//  Copyright © 2018 steelhaus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FunCorpAnatolyTestProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FunCorpAnatolyTestProject
+//
+//  Created by Анатолий Карельский on 06.09.18.
+//  Copyright © 2018 steelhaus. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
